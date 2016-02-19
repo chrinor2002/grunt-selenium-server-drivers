628300b98d2d1f450b38d47223e7184babd90ca4
Chris Reid <chrinor2002@gmail.com>
2016-02-19 13:55:49 -0700

    intial commit of working code

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # grunt-selenium-server-drivers
-Automatically downloads selenium server drivers to work with grunt-selenium-server
+
+> Start/stop a local Selenium standalon server.
+
+Grunt config example (with default options):
+```js
+module.exports = function (grunt) {
+  grunt.loadNpmTasks('grunt-selenium-server');
+  grunt.loadNpmTasks('grunt-selenium-server-drivers');
+  var os = require('os');
+  os.arch();
+  grunt.initConfig({
+    'selenium-server-drivers': {
+      dev: {
+        options: {
+          chromedriver: {
+            downloadUrl: 'http://chromedriver.storage.googleapis.com/2.21/chromedriver_linux64.zip',
+            downloadLocation: '<%= start-selenium-server.dev.options.downloadLocation %>',
+          }
+        }
+      }
+    },
+    'start-selenium-server': {
+      dev: {
+        options: {
+          autostop: false,
+          downloadUrl: 'https://selenium-release.storage.googleapis.com/2.46/selenium-server-standalone-2.46.0.jar',
+          downloadLocation: os.tmpdir(),
+          serverOptions: {},
+          systemProperties: {}
+        }
+      }
+    },
+    'stop-selenium-server': {
+      dev: {}
+    }
+  });
+};
+```
+
+## Supported Drivers
+chromedriver
---
+++ tasks/selenium_server_drivers.js
@@ -0,0 +1,97 @@
+var fs = require('fs');
+var os = require('os');
+var path = require('path');
+var url = require('url');
+var request = require('request');
+var ProgressBar = require('progress');
+
+module.exports = function (grunt) {
+
+  function getTarget(options) {
+    return path.join(options.downloadLocation, path.basename(options.downloadUrl));
+  }
+
+  function downloadDriver (options, cb) {
+    // Where to save jar to.
+    var target_destination = getTarget(options);
+
+    // If it's already there don't download it.
+    if (fs.existsSync(target_destination)) {
+      return cb(target_destination, null);
+    }
+
+    grunt.log.ok('Saving driver to: ' + target_destination);
+
+    var writeStream = fs.createWriteStream(target_destination);
+
+    // When the writeStream.end() resolves it will trigger a finish event.
+    // We have to wait for this before we know everything has been flushed to the filesystem.
+    // https://nodejs.org/api/stream.html#stream_writable_end_chunk_encoding_callback
+    writeStream.on('finish', function(){
+      grunt.log.ok('done.');
+      cb(target_destination, null);
+    });
+
+    // Start downloading and showing progress.
+    request(options.downloadUrl).on('response', function (res) {
+      if(res.statusCode >= 400) {
+          fs.unlink(target_destination, function (err) {
+            if (err)
+              grunt.log.error(err);
+            cb(null, new Error(options.downloadUrl + " returns " + res.statusCode));
+          })
+          return;
+      }
+      // Full length of file.
+      var len = parseInt(res.headers['content-length'], 10);
+
+      // Super nifty progress bar.
+      var bar = new ProgressBar(' downloading [:bar] :percent :etas', {
+        complete: '=',
+        incomplete: ' ',
+        width: 20,
+        total: len
+      });
+
+      // Write new data to file.
+      res.on('data', function (chunk) {
+        writeStream.write(chunk);
+        bar.tick(chunk.length);
+      });
+
+      // Tell the write stream that we are done calling write.
+      // The writeStream will emit a 'finish' event when everything has been written to the filesystem.
+      res.on('end', function () {
+        writeStream.end();
+      });
+
+      // Download error.
+      res.on('error', function (err) {
+        cb(null, err);
+      });
+    });
+  }
+
+  /**
+   * Start a Selenium server.
+   */
+  grunt.registerMultiTask('selenium-server-drivers', 'Download (if needed) driver files.', function () {
+    var done = this.async();
+    var target = this.target;
+
+    // Set default options.
+    var options = this.options({});
+
+    grunt.verbose.writeflags(options, 'Options');
+
+    for(var driver in options){
+      // Download
+      downloadDriver(options[driver], function (file, err) {
+        if (err) {
+          grunt.log.error(err);
+          return done(false);
+        }
+      });
+    }
+  });
+};
